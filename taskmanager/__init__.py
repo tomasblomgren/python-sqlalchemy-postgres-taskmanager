5513a33b6d65298292949299e345f211380ce26c
tomasblomgren <Tomas.blomgren96@gmail.com>
2022-10-31 18:48:01 +0000

    uppdating __init__.py

---
+++ taskmanager/__init__.py
@@ -0,0 +1,18 @@
+import os
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+if os.path.exists("env.py"):
+    import env  # noqa
+
+
+app = Flask(__name__)
+app.config["SECRET_KEY"] = os.environ.get("SECRET_KEY")
+
+if os.environ.get("DEVELOPMENT") == "True":
+    app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DB_URL")
+else:
+    app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DATABASE_URL")
+
+db = SQLAlchemy(app)
+
+from taskmanager import routes  # noqa
